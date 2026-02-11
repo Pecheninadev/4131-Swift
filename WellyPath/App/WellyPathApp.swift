f4397c80db4452b3b9949a6dc0815a1ea3f0d311
SBabchukTM <babchuk.serhii@trident-media.agency>
2026-02-11 12:20:22 +0200

    init

---
+++ WellyPath/App/WellyPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellyPathApp.swift
+//  WellyPath
+//
+//  Created by Serhii Babchuk on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct WellyPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WPLaunchView()
+        }
+    }
+}
